1eca19ccd33c48761ae3e647a3fb3f386d3e8526
Carl Boije <boijec@gmail.com>
2024-09-22 18:01:37 +0200

    chore: updating data model

--- .db/init.sql
+++ .db/init.sql
@@ -2,19 +2,7 @@ CREATE USER auth_mantle_manager WITH PASSWORD 'dudde';
 CREATE SCHEMA IF NOT EXISTS authmantledb;
 GRANT ALL ON SCHEMA authmantledb TO auth_mantle_manager;
 
-CREATE TABLE IF NOT EXISTS authmantledb.us_share (
-    id SERIAL PRIMARY KEY,
-    share_name VARCHAR(100) NOT NULL,
-
-    updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
-    updated_by VARCHAR(100) NOT NULL,
-    registered_at TIMESTAMP NOT NULL DEFAULT NOW(),
-    registered_by VARCHAR(100) NOT NULL
-);
-GRANT INSERT, UPDATE, DELETE, SELECT ON authmantledb.us_share TO auth_mantle_manager;
-GRANT USAGE, SELECT, UPDATE ON SEQUENCE authmantledb.us_share_id_seq TO auth_mantle_manager;
-
-CREATE TABLE IF NOT EXISTS authmantledb.us_role (
+CREATE TABLE IF NOT EXISTS authmantledb.role (
     id SERIAL PRIMARY KEY,
     role_name VARCHAR(100) NOT NULL,
     role_description VARCHAR(100) NOT NULL,
@@ -24,10 +12,10 @@ CREATE TABLE IF NOT EXISTS authmantledb.us_role (
     registered_at TIMESTAMP NOT NULL DEFAULT NOW(),
     registered_by VARCHAR(100) NOT NULL
 );
-GRANT INSERT, UPDATE, DELETE, SELECT ON authmantledb.us_role TO auth_mantle_manager;
-GRANT USAGE, SELECT, UPDATE ON SEQUENCE authmantledb.us_role_id_seq TO auth_mantle_manager;
+GRANT INSERT, UPDATE, DELETE, SELECT ON authmantledb.role TO auth_mantle_manager;
+GRANT USAGE, SELECT, UPDATE ON SEQUENCE authmantledb.role_id_seq TO auth_mantle_manager;
 
-CREATE TABLE IF NOT EXISTS authmantledb.us_country (
+CREATE TABLE IF NOT EXISTS authmantledb.country (
     id SERIAL PRIMARY KEY,
     country_name VARCHAR(100) NOT NULL,
     country_alpha_name VARCHAR(3) NOT NULL,
@@ -39,10 +27,10 @@ CREATE TABLE IF NOT EXISTS authmantledb.us_country (
     registered_at TIMESTAMP NOT NULL DEFAULT NOW(),
     registered_by VARCHAR(100) NOT NULL
 );
-GRANT INSERT, UPDATE, DELETE, SELECT ON authmantledb.us_country TO auth_mantle_manager;
-GRANT USAGE, SELECT, UPDATE ON SEQUENCE authmantledb.us_country_id_seq TO auth_mantle_manager;
+GRANT INSERT, UPDATE, DELETE, SELECT ON authmantledb.country TO auth_mantle_manager;
+GRANT USAGE, SELECT, UPDATE ON SEQUENCE authmantledb.country_id_seq TO auth_mantle_manager;
 
-CREATE TABLE IF NOT EXISTS authmantledb.us_realm (
+CREATE TABLE IF NOT EXISTS authmantledb.realm (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
 
@@ -51,10 +39,10 @@ CREATE TABLE IF NOT EXISTS authmantledb.us_realm (
     registered_at TIMESTAMP NOT NULL DEFAULT NOW(),
     registered_by VARCHAR(100) NOT NULL
 );
-GRANT INSERT, UPDATE, DELETE, SELECT ON authmantledb.us_user TO auth_mantle_manager;
-GRANT USAGE, SELECT, UPDATE ON SEQUENCE authmantledb.us_user_id_seq TO auth_mantle_manager;
+GRANT INSERT, UPDATE, DELETE, SELECT ON authmantledb.realm TO auth_mantle_manager;
+GRANT USAGE, SELECT, UPDATE ON SEQUENCE authmantledb.realm_id_seq TO auth_mantle_manager;
 
-CREATE TABLE IF NOT EXISTS authmantledb.us_user (
+CREATE TABLE IF NOT EXISTS authmantledb.user (
     id SERIAL PRIMARY KEY,
     username VARCHAR(100) NOT NULL,
     realm_id INTEGER NOT NULL,
@@ -67,21 +55,20 @@ CREATE TABLE IF NOT EXISTS authmantledb.us_user (
     city VARCHAR(50) NOT NULL,
     state VARCHAR(50),
     country_id INTEGER NOT NULL,
-    share_id INTEGER DEFAULT NULL,
 
     updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
     updated_by VARCHAR(100) NOT NULL,
     registered_at TIMESTAMP NOT NULL DEFAULT NOW(),
     registered_by VARCHAR(100) NOT NULL,
-    FOREIGN KEY (share_id) REFERENCES authmantledb.us_share(id),
-    FOREIGN KEY (role_id) REFERENCES authmantledb.us_role(id),
-    FOREIGN KEY (country_id) REFERENCES authmantledb.us_country(id),
-    FOREIGN KEY (realm_id) REFERENCES authmantledb.us_realm(id)
-);
-GRANT INSERT, UPDATE, DELETE, SELECT ON authmantledb.us_user TO auth_mantle_manager;
-GRANT USAGE, SELECT, UPDATE ON SEQUENCE authmantledb.us_user_id_seq TO auth_mantle_manager;
 
-CREATE TABLE IF NOT EXISTS authmantledb.us_session (
+    FOREIGN KEY (role_id) REFERENCES authmantledb.role(id),
+    FOREIGN KEY (country_id) REFERENCES authmantledb.country(id),
+    FOREIGN KEY (realm_id) REFERENCES authmantledb.realm(id)
+);
+GRANT INSERT, UPDATE, DELETE, SELECT ON authmantledb.user TO auth_mantle_manager;
+GRANT USAGE, SELECT, UPDATE ON SEQUENCE authmantledb.user_id_seq TO auth_mantle_manager;
+
+CREATE TABLE IF NOT EXISTS authmantledb.session (
     id SERIAL PRIMARY KEY,
     session_id UUID NOT NULL,
     user_ref INTEGER NOT NULL,
@@ -94,62 +81,116 @@ CREATE TABLE IF NOT EXISTS authmantledb.us_session (
     updated_by VARCHAR(100) NOT NULL,
     registered_at TIMESTAMP NOT NULL DEFAULT NOW(),
     registered_by VARCHAR(100) NOT NULL,
-    FOREIGN KEY (user_ref) REFERENCES authmantledb.us_user(id)
+
+    FOREIGN KEY (user_ref) REFERENCES authmantledb.user(id)
 );
-GRANT INSERT, UPDATE, DELETE, SELECT ON authmantledb.us_session TO auth_mantle_manager;
-GRANT USAGE, SELECT, UPDATE ON SEQUENCE authmantledb.us_session_id_seq TO auth_mantle_manager;
+GRANT INSERT, UPDATE, DELETE, SELECT ON authmantledb.session TO auth_mantle_manager;
+GRANT USAGE, SELECT, UPDATE ON SEQUENCE authmantledb.session_id_seq TO auth_mantle_manager;
 
 -- TABLES FOR INTERNALS
-CREATE TABLE IF NOT EXISTS authmantledb.in_auth_code_requests (
+CREATE TABLE IF NOT EXISTS authmantledb.auth_code_requests (
     id SERIAL PRIMARY KEY,
     user_id INTEGER NOT NULL,
     auth_code UUID NOT NULL DEFAULT gen_random_uuid(),
     valid_until TIMESTAMP NOT NULL DEFAULT NOW() + (15 * INTERVAL '1 minute'),
     consumed INTEGER NOT NULL DEFAULT 0,
 
-    FOREIGN KEY (user_id) REFERENCES authmantledb.us_user(id)
-);
-GRANT INSERT, UPDATE, DELETE, SELECT ON authmantledb.in_auth_code_requests TO auth_mantle_manager;
-GRANT USAGE, SELECT, UPDATE ON SEQUENCE authmantledb.in_auth_code_requests_id_seq TO auth_mantle_manager;
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_by VARCHAR(100) NOT NULL,
+    registered_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    registered_by VARCHAR(100) NOT NULL,
 
-CREATE TABLE IF NOT EXISTS authmantledb.in_supp_auth_audience (
-    id SERIAL PRIMARY KEY,
-    audience VARCHAR(200) NOT NULL
+    FOREIGN KEY (user_id) REFERENCES authmantledb.user(id)
 );
-GRANT INSERT, UPDATE, DELETE, SELECT ON authmantledb.in_supp_auth_audience TO auth_mantle_manager;
-GRANT USAGE, SELECT, UPDATE ON SEQUENCE authmantledb.in_supp_auth_audience_id_seq TO auth_mantle_manager;
+GRANT INSERT, UPDATE, DELETE, SELECT ON authmantledb.auth_code_requests TO auth_mantle_manager;
+GRANT USAGE, SELECT, UPDATE ON SEQUENCE authmantledb.auth_code_requests_id_seq TO auth_mantle_manager;
 
-CREATE TABLE IF NOT EXISTS authmantledb.in_supp_auth_grant_types (
+CREATE TABLE IF NOT EXISTS authmantledb.supp_auth_audience (
     id SERIAL PRIMARY KEY,
-    grant_type VARCHAR(200) NOT NULL
-);
-GRANT INSERT, UPDATE, DELETE, SELECT ON authmantledb.in_supp_auth_grant_types TO auth_mantle_manager;
-GRANT USAGE, SELECT, UPDATE ON SEQUENCE authmantledb.in_supp_auth_grant_types_id_seq TO auth_mantle_manager;
+    audience VARCHAR(200) NOT NULL,
+    realm_id INTEGER NOT NULL,
 
-CREATE TABLE IF NOT EXISTS authmantledb.in_supp_auth_scopes (
-    id SERIAL PRIMARY KEY,
-    scope VARCHAR(200) NOT NULL
-);
-GRANT INSERT, UPDATE, DELETE, SELECT ON authmantledb.in_supp_auth_scopes TO auth_mantle_manager;
-GRANT USAGE, SELECT, UPDATE ON SEQUENCE authmantledb.in_supp_auth_scopes_id_seq TO auth_mantle_manager;
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_by VARCHAR(100) NOT NULL,
+    registered_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    registered_by VARCHAR(100) NOT NULL,
 
-CREATE TABLE IF NOT EXISTS authmantledb.in_supp_auth_claims (
-    id SERIAL PRIMARY KEY,
-    claim VARCHAR(200) NOT NULL
+    FOREIGN KEY (realm_id) REFERENCES authmantledb.realm(id)
 );
-GRANT INSERT, UPDATE, DELETE, SELECT ON authmantledb.in_supp_auth_claims TO auth_mantle_manager;
-GRANT USAGE, SELECT, UPDATE ON SEQUENCE authmantledb.in_supp_auth_claims_id_seq TO auth_mantle_manager;
+GRANT INSERT, UPDATE, DELETE, SELECT ON authmantledb.supp_auth_audience TO auth_mantle_manager;
+GRANT USAGE, SELECT, UPDATE ON SEQUENCE authmantledb.supp_auth_audience_id_seq TO auth_mantle_manager;
 
-CREATE TABLE IF NOT EXISTS authmantledb.in_supp_auth_subject_types (
+CREATE TABLE IF NOT EXISTS authmantledb.supp_auth_grant_types (
     id SERIAL PRIMARY KEY,
-    subject_type VARCHAR(200) NOT NULL
-);
-GRANT INSERT, UPDATE, DELETE, SELECT ON authmantledb.in_supp_auth_subject_types TO auth_mantle_manager;
-GRANT USAGE, SELECT, UPDATE ON SEQUENCE authmantledb.in_supp_auth_subject_types_id_seq TO auth_mantle_manager;
+    grant_type VARCHAR(200) NOT NULL,
+    realm_id INTEGER NOT NULL,
 
-CREATE TABLE IF NOT EXISTS authmantledb.in_supp_auth_allowed_redirects (
-    id SERIAL PRIMARY KEY,
-    redirect_uri VARCHAR(200) NOT NULL
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_by VARCHAR(100) NOT NULL,
+    registered_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    registered_by VARCHAR(100) NOT NULL,
+
+    FOREIGN KEY (realm_id) REFERENCES authmantledb.realm(id)
 );
-GRANT INSERT, UPDATE, DELETE, SELECT ON authmantledb.in_supp_auth_allowed_redirects TO auth_mantle_manager;
-GRANT USAGE, SELECT, UPDATE ON SEQUENCE authmantledb.in_supp_auth_allowed_redirects_id_seq TO auth_mantle_manager;
+GRANT INSERT, UPDATE, DELETE, SELECT ON authmantledb.supp_auth_grant_types TO auth_mantle_manager;
+GRANT USAGE, SELECT, UPDATE ON SEQUENCE authmantledb.supp_auth_grant_types_id_seq TO auth_mantle_manager;
+
+CREATE TABLE IF NOT EXISTS authmantledb.supp_auth_scopes (
+    id SERIAL PRIMARY KEY,
+    scope VARCHAR(200) NOT NULL,
+    realm_id INTEGER NOT NULL,
+
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_by VARCHAR(100) NOT NULL,
+    registered_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    registered_by VARCHAR(100) NOT NULL,
+
+    FOREIGN KEY (realm_id) REFERENCES authmantledb.realm(id)
+);
+GRANT INSERT, UPDATE, DELETE, SELECT ON authmantledb.supp_auth_scopes TO auth_mantle_manager;
+GRANT USAGE, SELECT, UPDATE ON SEQUENCE authmantledb.supp_auth_scopes_id_seq TO auth_mantle_manager;
+
+CREATE TABLE IF NOT EXISTS authmantledb.supp_auth_claims (
+    id SERIAL PRIMARY KEY,
+    claim VARCHAR(200) NOT NULL,
+    realm_id INTEGER NOT NULL,
+
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_by VARCHAR(100) NOT NULL,
+    registered_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    registered_by VARCHAR(100) NOT NULL,
+
+    FOREIGN KEY (realm_id) REFERENCES authmantledb.realm(id)
+);
+GRANT INSERT, UPDATE, DELETE, SELECT ON authmantledb.supp_auth_claims TO auth_mantle_manager;
+GRANT USAGE, SELECT, UPDATE ON SEQUENCE authmantledb.supp_auth_claims_id_seq TO auth_mantle_manager;
+
+CREATE TABLE IF NOT EXISTS authmantledb.supp_auth_subject_types (
+    id SERIAL PRIMARY KEY,
+    subject_type VARCHAR(200) NOT NULL,
+    realm_id INTEGER NOT NULL,
+
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_by VARCHAR(100) NOT NULL,
+    registered_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    registered_by VARCHAR(100) NOT NULL,
+
+    FOREIGN KEY (realm_id) REFERENCES authmantledb.realm(id)
+);
+GRANT INSERT, UPDATE, DELETE, SELECT ON authmantledb.supp_auth_subject_types TO auth_mantle_manager;
+GRANT USAGE, SELECT, UPDATE ON SEQUENCE authmantledb.supp_auth_subject_types_id_seq TO auth_mantle_manager;
+
+CREATE TABLE IF NOT EXISTS authmantledb.supp_auth_allowed_redirects (
+    id SERIAL PRIMARY KEY,
+    redirect_uri VARCHAR(200) NOT NULL,
+    realm_id INTEGER NOT NULL,
+
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_by VARCHAR(100) NOT NULL,
+    registered_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    registered_by VARCHAR(100) NOT NULL,
+
+    FOREIGN KEY (realm_id) REFERENCES authmantledb.realm(id)
+);
+GRANT INSERT, UPDATE, DELETE, SELECT ON authmantledb.supp_auth_allowed_redirects TO auth_mantle_manager;
+GRANT USAGE, SELECT, UPDATE ON SEQUENCE authmantledb.supp_auth_allowed_redirects_id_seq TO auth_mantle_manager;
